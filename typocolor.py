9180eb53edcdf0ecdae6cb0aa214bf92d7b74950
cako <golden.cako+github@gmail.com>
2013-04-06 01:58:35 -0300

    Add overlay.

--- typocolor.py
+++ typocolor.py
@@ -5,6 +5,7 @@ import Image
 import numpy as np
 
 def PIL_to_numpy(img_PIL):
+    #return np.asarray(img_PIL).astype(np.uint8)
     return np.array(img_PIL.getdata()).reshape(img_PIL.size[::-1])
 
 def get_line_limits(img):
@@ -50,3 +51,9 @@ if __name__== "__main__":
     m_img = mean_image(img, limits)
     m_img_PIL = Image.fromarray(m_img)
     m_img_PIL.save('mean_' + sys.argv[1])
+
+    img_PIL = img_PIL.convert("RGBA")
+    m_img_PIL = m_img_PIL.convert("RGBA")
+    overlay_PIL = Image.blend(m_img_PIL, img_PIL, 0.5)
+    overlay_PIL.save('overlay_' + sys.argv[1])
+
